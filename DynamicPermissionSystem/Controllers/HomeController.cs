ccb65999676d92a3f41ff53e7b43a4680630a938
Firoz Ahmed <firoz.sw@perkyrabbit.com>
2025-10-29 16:04:07 +0600

    layout design change and report controller some change

--- DynamicPermissionSystem/Controllers/HomeController.cs
+++ DynamicPermissionSystem/Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DynamicPermissionSystem.Models;
 
@@ -6,13 +5,27 @@ namespace DynamicPermissionSystem.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
         var userId = HttpContext.Session.GetInt32("UserId");
         if (userId == null)
             return RedirectToAction("Login", "Account");
 
+        // Fetch dynamic counts
+        ViewBag.TotalMenus = _context.Menus.Count();
+        ViewBag.TotalUsers = _context.Users.Count();
+        ViewBag.TotalRoles = _context.Roles.Count();
+        ViewBag.TotalPermissions = _context.RoleMenuPermissions.Count();
+
+        ViewBag.UserName = HttpContext.Session.GetString("UserName") ?? "User";
+
         return View();
     }
-
 }
--- DynamicPermissionSystem/Controllers/ReportController.cs
+++ DynamicPermissionSystem/Controllers/ReportController.cs
@@ -1,11 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using DynamicPermissionSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DynamicPermissionSystem.Controllers
 {
     public class ReportController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ReportController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            // 🔹 Get role from session
+            var roleId = HttpContext.Session.GetInt32("RoleId") ?? 0;
+
+            // 🔹 Check permission for this menu
+            var hasPermission = _db.RoleMenuPermissions
+                                   .Include(rp => rp.Menu)
+                                   .Any(rp => rp.RoleId == roleId
+                                           && rp.CanView
+                                           && rp.Menu.Controller == "Report"
+                                           && rp.Menu.Action == "Index");
+
+            if (!hasPermission)
+            {
+                // Redirect to home or show "Access Denied"
+                return RedirectToAction("AccessDenied", "Home");
+            }
+
             return View();
         }
     }
